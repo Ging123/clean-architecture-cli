4d05e05ee52143af9d524bdccc38e4217a17f7a0
Ging123 <iceglauber@outlook.com>
2022-01-25 23:13:31 -0300

    add system to create model

---
+++ bin/createModel.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+"use strict";
+
+const turnFirstLetterUpperCase = require('../util/turnFirstLetterUpperCase');
+const readline  = require('readline');
+const shell = require("shelljs");
+var modelName = '';
+var modelNameForClass = '';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Your model name: ', (name) => {
+  setModelName(name);
+  enterInModelFolder();
+  createModelFile();
+  addTextToFile();
+  rl.close();
+});
+
+const setModelName = name => {
+  modelName = `${name}Model`;
+  modelNameForClass = turnFirstLetterUpperCase(modelName)
+}
+
+const enterInModelFolder = () => shell.cd('src/models/');
+
+const createModelFile = () => shell.touch(`${modelName}.ts`);
+
+function addTextToFile() {
+  shell.exec(`echo class ${modelNameForClass} { >> ${modelName}.ts`);
+  shell.exec(`echo } >> ${modelName}.ts`);
+  shell.exec(`echo export default ${modelNameForClass}; >> ${modelName}.ts`);
+}
